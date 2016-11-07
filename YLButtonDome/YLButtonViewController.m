4d2c6e86a18f31ee8c1be2a1d6dd5e415d75625f
张银龙 <469891897@qq.com>
2016-11-07 23:57:03 +0800

    完善 dome

--- YLButtonDome/YLButtonViewController.m
+++ YLButtonDome/YLButtonViewController.m
@@ -10,28 +10,125 @@
 
 @interface YLButtonViewController ()
 
+@property (weak, nonatomic) IBOutlet YLButton *itemButton;
+
+@property (nonatomic, assign) NSInteger imageInsetsTop;
+@property (nonatomic, assign) NSInteger imageInsetsLeft;
+@property (nonatomic, assign) NSInteger imageInsetsBottom;
+@property (nonatomic, assign) NSInteger imageInsetsRight;
+
+@property (nonatomic, assign) NSInteger titleInsetsTop;
+@property (nonatomic, assign) NSInteger titleInsetsLeft;
+@property (nonatomic, assign) NSInteger titleInsetsBottom;
+@property (nonatomic, assign) NSInteger titleInsetsRight;
+
 @end
 
 @implementation YLButtonViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    
+    self.itemButton.imageAlignment = self.imageAlignment;
+    self.itemButton.titleLabel.backgroundColor = [UIColor cyanColor];
+    
+    self.imageInsetsTop = 0;
+    self.imageInsetsLeft = 0;
+    self.imageInsetsBottom = 0;
+    self.imageInsetsRight = 0;
+    
+    self.titleInsetsTop = 0;
+    self.titleInsetsLeft = 0;
+    self.titleInsetsBottom = 0;
+    self.titleInsetsRight = 0;
+ 
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - yl_imageInsets
+
+- (IBAction)imageInsetsAddDidClick:(UIButton *)sender{
+    
+    switch (sender.tag) {
+        case 0:
+            self.imageInsetsTop++;
+            break;
+        case 1:
+            self.imageInsetsLeft++;
+            break;
+        case 2:
+            self.imageInsetsBottom++;
+            break;
+        case 3:
+            self.imageInsetsRight++;
+            break;
+        default:
+            break;
+    }
+    self.itemButton.yl_imageInsets = UIEdgeInsetsMake(self.imageInsetsTop, self.imageInsetsLeft, self.imageInsetsBottom, self.imageInsetsRight);
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)imageInsetsSubtractDidClick:(UIButton *)sender{
+    
+    switch (sender.tag) {
+        case 0:
+            self.imageInsetsTop--;
+            break;
+        case 1:
+            self.imageInsetsLeft--;
+            break;
+        case 2:
+            self.imageInsetsBottom--;
+            break;
+        case 3:
+            self.imageInsetsRight--;
+            break;
+        default:
+            break;
+    }
+    self.itemButton.yl_imageInsets = UIEdgeInsetsMake(self.imageInsetsTop, self.imageInsetsLeft, self.imageInsetsBottom, self.imageInsetsRight);
 }
-*/
+#pragma mark - yl_titleInsets
+
+- (IBAction)titleInsetsAddDidClick:(UIButton *)sender{
+    
+    switch (sender.tag) {
+        case 0:
+            self.titleInsetsTop++;
+            break;
+        case 1:
+            self.titleInsetsLeft++;
+            break;
+        case 2:
+            self.titleInsetsBottom++;
+            break;
+        case 3:
+            self.titleInsetsRight++;
+            break;
+        default:
+            break;
+    }
+    self.itemButton.yl_titleInsets = UIEdgeInsetsMake(self.titleInsetsTop, self.titleInsetsLeft, self.titleInsetsBottom, self.titleInsetsRight);
+}
+
+- (IBAction)titleInsetsSubtractDidClick:(UIButton *)sender{
+    
+    switch (sender.tag) {
+        case 0:
+            self.titleInsetsTop--;
+            break;
+        case 1:
+            self.titleInsetsLeft--;
+            break;
+        case 2:
+            self.titleInsetsBottom--;
+            break;
+        case 3:
+            self.titleInsetsRight--;
+            break;
+        default:
+            break;
+    }
+    self.itemButton.yl_titleInsets = UIEdgeInsetsMake(self.titleInsetsTop, self.titleInsetsLeft, self.titleInsetsBottom, self.titleInsetsRight);
+}
 
 @end
